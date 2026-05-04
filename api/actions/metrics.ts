caac3e7c0315fdb3266b22caba3996fbe2351904
Eric (Linka) <eric@agencialinka.com.br>
2026-05-04 17:26:29 -0300

    .

--- api/actions/metrics.ts
+++ api/actions/metrics.ts
@@ -2,6 +2,7 @@ import { handleAction, queryStringValue } from "../_actions";
 
 export default async function handler(request: any, response: any) {
   await handleAction(request, response, {
-    GET: (service) => service.metrics(queryStringValue(request, "month"))
+    GET: (service) => service.metrics(queryStringValue(request, "month")),
+    POST: (service, body) => service.metrics((body as { month?: string }).month)
   });
 }
